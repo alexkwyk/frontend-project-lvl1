02c60deb6dcfdd1acbfa96662de5f69db9edfc72
alexey.anisimov <alexkwyk@gmail.com>
2022-04-26 01:30:19 +0300

    refactoring gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,44 +1,40 @@
 import checkAnswers, { gameCount } from '../index.js';
 import getRandomNumber from '../utils.js';
 
-export default () => {
-  const description = 'Find the greatest common divisor of given numbers.';
+const description = 'Find the greatest common divisor of given numbers.';
 
+export default () => {
   const questions = [];
   const answers = [];
 
-  const gcdGame = (count) => {
-    if (count > 2) {
-      return;
-    }
-    let a = getRandomNumber(1, 99);
-    let b = getRandomNumber(1, 99);
-
-    let gameCorrectAnswer;
-
-    const findAnswer = (num) => {
-      if (num > 99) {
-        if (gameCorrectAnswer === undefined) {
-          a = getRandomNumber(1, 99);
-          b = getRandomNumber(1, 99);
-          findAnswer(2);
-        }
-        return;
-      }
+  const findAnswer = (a, b) => {
+    let result = 0;
+    for (let num = 2; num < 100; num += 1) {
       if (a % num === 0 && b % num === 0) {
-        gameCorrectAnswer = num;
-        findAnswer(num + 1);
+        result = num;
       }
-      findAnswer(num + 1);
-    };
-
-    findAnswer(2);
-    const gameQuestion = `${a} ${b}`;
-    questions.push(gameQuestion);
-    answers.push(String(gameCorrectAnswer));
-    gcdGame(count + 1);
+    }
+    return result;
   };
 
-  gcdGame(0);
+  const getCorrectNumbers = () => {
+    let num1 = getRandomNumber(1, 99);
+    let num2 = getRandomNumber(1, 99);
+    const checkAnswer = findAnswer(num1, num2) === 0;
+    if (checkAnswer === true) {
+      num1 = getRandomNumber(1, 99);
+      num2 = getRandomNumber(1, 99);
+      return getCorrectNumbers(num1, num2);
+    }
+    return [num1, num2];
+  };
+
+  for (let i = 0; i < gameCount; i += 1) {
+    const Numbers = getCorrectNumbers();
+    const gameQuestion = `${Numbers[0]} ${Numbers[1]}`;
+    const gameCorrectAnswer = findAnswer(Numbers[0], Numbers[1]);
+    questions.push(gameQuestion);
+    answers.push(String(gameCorrectAnswer));
+  }
   checkAnswers(questions, answers, description);
 };
