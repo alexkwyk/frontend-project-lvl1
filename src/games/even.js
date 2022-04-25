45fd960c4b678f5aa8b3a67e4932a263c792e2a2
alexey.anisimov <alexkwyk@gmail.com>
2022-04-26 00:01:05 +0300

    refactoring even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,22 +1,17 @@
 import checkAnswers from '../index.js';
+import getRandomNumber from '../utils.js';
+
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 export default () => {
-  const description = 'Answer "yes" if the number is even, otherwise answer "no".';
-  const getRandomNumber = (min, max) => Math.floor(Math.random() * max) + min;
   const questions = [];
   const answers = [];
 
-  const evenGame = (count) => {
-    if (count > 2) {
-      return;
-    }
+  for (let i = 0; i < 3; i += 1) {
     const gameQuestion = getRandomNumber(1, 99);
     const gameCorrectAnswer = (gameQuestion % 2 === 0) ? 'yes' : 'no';
     questions.push(gameQuestion);
     answers.push(gameCorrectAnswer);
-    evenGame(count + 1);
-  };
-
-  evenGame(0);
+  }
   checkAnswers(questions, answers, description);
 };
