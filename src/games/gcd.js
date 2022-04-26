90bd38ebd2ba3fb2275f095a91edaa3e35701583
alexey.anisimov <alexkwyk@gmail.com>
2022-04-26 13:06:53 +0300

    update gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,36 +3,36 @@ import getRandomNumber from '../utils.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
+const calcGCD = (a, b) => {
+  let result = 0;
+  for (let num = 2; num < 100; num += 1) {
+    if (a % num === 0 && b % num === 0) {
+      result = num;
+    }
+  }
+  return result;
+};
+
+const getCorrectNumbers = () => {
+  let num1 = getRandomNumber(1, 99);
+  let num2 = getRandomNumber(1, 99);
+  const checkAnswer = calcGCD(num1, num2) === 0;
+  if (checkAnswer === true) {
+    num1 = getRandomNumber(1, 99);
+    num2 = getRandomNumber(1, 99);
+    return getCorrectNumbers(num1, num2);
+  }
+  return [num1, num2];
+};
+
 export default () => {
   const questions = [];
   const answers = [];
 
-  const findAnswer = (a, b) => {
-    let result = 0;
-    for (let num = 2; num < 100; num += 1) {
-      if (a % num === 0 && b % num === 0) {
-        result = num;
-      }
-    }
-    return result;
-  };
-
-  const getCorrectNumbers = () => {
-    let num1 = getRandomNumber(1, 99);
-    let num2 = getRandomNumber(1, 99);
-    const checkAnswer = findAnswer(num1, num2) === 0;
-    if (checkAnswer === true) {
-      num1 = getRandomNumber(1, 99);
-      num2 = getRandomNumber(1, 99);
-      return getCorrectNumbers(num1, num2);
-    }
-    return [num1, num2];
-  };
-
   for (let i = 0; i < gameCount; i += 1) {
     const Numbers = getCorrectNumbers();
     const gameQuestion = `${Numbers[0]} ${Numbers[1]}`;
-    const gameCorrectAnswer = findAnswer(Numbers[0], Numbers[1]);
+    const gameCorrectAnswer = calcGCD(Numbers[0], Numbers[1]);
     questions.push(gameQuestion);
     answers.push(String(gameCorrectAnswer));
   }
