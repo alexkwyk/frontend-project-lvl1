8c139726fbd4a6f37e4944d10266cfa283d815cf
alexey.anisimov <alexkwyk@gmail.com>
2022-04-30 23:16:24 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ export const gameCount = 3;
 export default (getGameValues, description) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}}!`);
+  console.log(`Hello, ${userName}!`);
   console.log(`${description}`);
 
   for (let gameRound = 0; gameRound < gameCount; gameRound += 1) {
@@ -16,10 +16,10 @@ export default (getGameValues, description) => {
     const isAnswerCorrect = (userAnswer.toLowerCase() === String(correctAnswer));
     if (isAnswerCorrect === false) {
       console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
-      console.log(`Let's try again, ${userName}}!`);
+      console.log(`Let's try again, ${userName}!`);
       return;
     }
     console.log('Correct!');
   }
-  console.log(`Congratulations, ${userName}}!`);
+  console.log(`Congratulations, ${userName}!`);
 };
