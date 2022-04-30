30cf49629fb6f1434a1e2c875fc1cc4480412b58
alexey.anisimov <alexkwyk@gmail.com>
2022-04-30 23:14:44 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -4,22 +4,22 @@ export const gameCount = 3;
 
 export default (getGameValues, description) => {
   console.log('Welcome to the Brain Games!');
-  const username = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${username}!`);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}}!`);
   console.log(`${description}`);
 
   for (let gameRound = 0; gameRound < gameCount; gameRound += 1) {
     const values = getGameValues();
-    const [question, answer] = values;
+    const [question, correctAnswer] = values;
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    const isAnswerCorrect = (userAnswer.toLowerCase() === String(answer));
+    const isAnswerCorrect = (userAnswer.toLowerCase() === String(correctAnswer));
     if (isAnswerCorrect === false) {
-      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${answer}".`);
-      console.log(`Let's try again, ${username}!`);
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
+      console.log(`Let's try again, ${userName}}!`);
       return;
     }
     console.log('Correct!');
   }
-  console.log(`Congratulations, ${username}!`);
+  console.log(`Congratulations, ${userName}}!`);
 };
