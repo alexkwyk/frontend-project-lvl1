0503501e6ffac55e0c8b41a264d14365c7e32283
alexey.anisimov <alexkwyk@gmail.com>
2022-05-01 22:01:43 +0300

    update gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,7 +5,8 @@ const description = 'Find the greatest common divisor of given numbers.';
 
 const calculateGreatestCommonDivisor = (a, b) => {
   let result = 0;
-  for (let divisor = 2; divisor < 100; divisor += 1) {
+  const maxDivisor = (a > b) ? a : b;
+  for (let divisor = 1; divisor <= maxDivisor; divisor += 1) {
     if (a % divisor === 0 && b % divisor === 0) {
       result = divisor;
     }
@@ -13,18 +14,14 @@ const calculateGreatestCommonDivisor = (a, b) => {
   return result;
 };
 
-const getGameValues = () => {
+const getRound = () => {
   const firstNumber = getRandomNumber(1, 99);
   const secondNumber = getRandomNumber(1, 99);
-  const isHaveCommonDivisor = calculateGreatestCommonDivisor(firstNumber, secondNumber) !== 0;
-  if (isHaveCommonDivisor === false) {
-    return getGameValues();
-  }
   const answer = calculateGreatestCommonDivisor(firstNumber, secondNumber);
   const question = `${firstNumber} ${secondNumber}`;
   return [question, answer];
 };
 
 export default () => {
-  startEngine(getGameValues, description);
+  startEngine(getRound, description);
 };
